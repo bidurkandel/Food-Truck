148a9484ece581d12cdb194b121134f3ae90545a
BK <bk@BKs-MacBook-Pro.local>
2021-04-02 09:36:51 -0700

    main

--- src/reducers/diner.js
+++ src/reducers/diner.js
@@ -21,7 +21,7 @@ const initialState = {
     username: "john",
     email: "john@john.com",
     password: "1234",
-    currentLocation: "SF",
+    currentLocation: "SD",
     favoriteTrucks: [],
   },
   trucks: [],
